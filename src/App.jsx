39dcb27505593b44c5aa441b04e3cdf2ade457f6
rishab1493 <rishab1493@gmail.com>
2025-05-25 10:13:14 +0530

    sos

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,5 @@
 import { Routes, HashRouter, Route } from "react-router-dom"
-import JsonGenerator from "./file/Json"
-import IfElse from "./file/IfElse"
+
 import CustomerNumber from "./file/CustomerNumber"
 
 function App() {
@@ -8,8 +7,6 @@ function App() {
     <>
       <HashRouter>
         <Routes>
-          <Route path="/json-generator" element={<JsonGenerator />} />
-          <Route path="/if-else" element={<IfElse />} />
           <Route path="*" element={<CustomerNumber />} />
         </Routes>
       </HashRouter>
