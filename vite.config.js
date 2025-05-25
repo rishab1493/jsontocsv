b02b2a409a9f3c4434e98ea5cceadcc65ed0744c
rishab1493 <rishab1493@gmail.com>
2025-05-25 09:56:43 +0530

    some changes

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from "@vitejs/plugin-react"
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: "https://rishab1493.github.io/jsontocsv/",
+  base: "jsontocsv",
   plugins: [react()],
 })
